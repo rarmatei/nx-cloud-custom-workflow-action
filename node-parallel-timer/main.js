908d7f76b52af2fb4584aaf8b0ddcfdd0b307ae0
Rares Matei <matei.rar@gmail.com>
2025-01-06 18:34:37 +0000

    wip

--- node-parallel-timer/main.js
+++ node-parallel-timer/main.js
@@ -1,3 +1,7 @@
 const {execSync} = require('child_process');
 
+console.log("sleep before: " + new Date())
+execSync('sleep 10')
 execSync(`for i in {1..10}; do date +"%H:%M:%S"; sleep 1; done`, {stdio: "inherit"})
+execSync('sleep 10')
+console.log("sleep after: " + new Date())
